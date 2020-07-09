e16728078b5e4689d193fb6c4bda97e4ebead0ae
douglaspujol <douglas.pujol@outlook.com>
2020-07-09 11:50:33 -0300

    Pagina 02 finalizada

--- src/pages/Planet/index.js
+++ src/pages/Planet/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/alt-text */
 
-// import { Container } from './styles';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, InfoPlanet, Population, Movies } from './styles';
 
 function Planet() {
-  return <h1>Planets</h1>;
+  return (
+    <Container>
+      <nav>
+        <ul>
+          <li>Home/</li>
+          <li>
+            <Link to="/">Planetas/</Link>
+          </li>
+          <li>Planet Name</li>
+        </ul>
+      </nav>
+      <div>
+        <img src={require(`../../assets/planets/2.jpg`)} />
+        <InfoPlanet>
+          <span>PLANET NAME</span>
+          <p>POPULATION: 100</p>
+          <p>ROTATION PERIOD: 10</p>
+          <p>ORBITAL PERIOD: 100</p>
+          <p>DIAMETER: 100</p>
+          <p>GRAVITY: 100</p>
+          <p>TERRAIN: 100</p>
+          <p>SURFACE WATER: 100</p>
+          <p>CLIMATE: 100</p>
+          <Population>
+            <span>Habitantes</span>
+            <p>- Nome</p>
+            <p>- Nome</p>
+            <p>- Nome</p>
+          </Population>
+          <Movies>
+            <span>Filmes</span>
+            <p>- Nome</p>
+            <p>- Nome</p>
+            <p>- Nome</p>
+          </Movies>
+        </InfoPlanet>
+      </div>
+    </Container>
+  );
 }
 
 export default Planet;
---
+++ src/pages/Planet/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+  nav ul {
+    display: flex;
+    li {
+      margin-right: 6px;
+      a {
+        color: inherit;
+      }
+    }
+  }
+  > div {
+    margin-top: 56px;
+    display: flex;
+    img {
+      width: 350px;
+      height: 350px;
+      margin-right: 25px;
+    }
+  }
+`;
+export const InfoPlanet = styled.div`
+  span {
+    margin-bottom: 16px;
+    font-size: 33px;
+  }
+`;
+export const Population = styled.div`
+  span {
+    margin-bottom: 16px;
+    font-size: 33px;
+  }
+`;
+export const Movies = styled.div`
+  span {
+    margin-bottom: 16px;
+    font-size: 33px;
+  }
+`;
